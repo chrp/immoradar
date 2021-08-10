b2cf4170181ff6546346473cbb59d158e2cb6780
chrp <hi@chrp.es>
2021-08-10 13:04:24 +0200

    bumps schema

--- db/schema.rb
+++ db/schema.rb
@@ -2,17 +2,20 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_08_02_084618) do
 
-  create_table "ads", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "ads", force: :cascade do |t|
     t.string "platform", null: false
     t.string "platform_key", null: false
     t.text "url", null: false
@@ -35,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_08_02_084618) do
     t.integer "floor"
     t.integer "price_per_sqm_flat"
     t.integer "price_per_sqm_property"
+    t.integer "price_per_room"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["is_favorite"], name: "index_ads_on_is_favorite"
